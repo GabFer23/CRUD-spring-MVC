03699c3665574482f996beccc73353bfba87ab6d
Gabriel Aristizabal <gabrielfernando200109@gmail.com>
2022-08-22 16:02:48 -0500

    first commit

---
+++ src/test/java/com/gestionproductos/GestionProductosApplicationTests.java
@@ -0,0 +1,10 @@
+package com.gestionproductos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionProductosApplicationTests {
+
+
+}
